c8b8b698f0e7434de719b6ea393e0c2cd7ec396e
Łukasz Woliński <lukasz.wolinski@partners.mbank.pl>
2021-04-07 15:28:05 +0200

    Added comments

--- Krzysiek/praca domowa v5 .py
+++ Krzysiek/praca domowa v5 .py
@@ -2,6 +2,7 @@
 
 #GLOBAL
 
+#Czy na pewno wszystkie te zmienne muszą być zmiennymi globalnymi? Nie wykorzystujesz niektórych tylko w funkcjach?
 hello_literal = ('Cześć')
 welcome_literal =('To jest program,')
 menu_present1 = ("COMBO trzy w jednym")
@@ -12,6 +13,8 @@ char_present1 = ('do zliczania znaków w stringu')
 null_literal = ("Nic nie wpisałeś.")
 validation_input_1 = ('Nie rozpoznano dyspozycji.')
 end_literal = ('Dziękuje za skorzystanie z programu')
+
+#Takie nazwy zmiennych mało mówią, nie wiadomo która do czego służy. 
 back_literal = ('lub cofnij do menu wisując "BACK"')
 back_literal_2 = ("Podaj element lub wyświelt wynik (RESULT) lub cofnij do menu (BACK)")
 back_literal_3 =("Podaj kolejny element lub wyświelt wynik (RESULT) lub cofnij do menu (BACK)")
@@ -19,6 +22,7 @@ back_literal_3 =("Podaj kolejny element lub wyświelt wynik (RESULT) lub cofnij 
 
 
 #MENU:
+#Np. to powinno być zdefiniowane w funkcji menu
 first_decision_literal = ('''Co chcesz zrobić?:
         1 - Okreśłić czy liczba jest parzysta
         2 - Stworzyć liste elementów (wersja 1)
@@ -43,6 +47,8 @@ next_number_error = ('Podaj tym razem liczbę.')
 next_number = ("Podaj kolejną cyfrę.")
 positive_literal = ('Liczba x jest parzysta') # [:6], [-13:]
 negative_literal = ('Liczba X jest nieparzysta')# [:6], [-16:]
+
+#Ponownie problem z nazwami i scopem (globalne vs lokalne) zmiennych
 error_literal =("Coś ty wpisał? \nTo nie jest liczba, tylko coś ze znakami specjalnymi. \nHieroglifów Ci nie zweryfikuje.")
 error_litera2 = ('Chcesz mnie oszukać spacją/tabulatorem?? \nNie ładnie kłamczuszku :P')
 error_litera3 = ("Coś ty wpisał? \nTo nie jest liczba. \nOperujesz na systemie rzymskim wpisując litery??")
@@ -109,8 +115,11 @@ def menu ():
     print()
 
     while True:
+        #Poszukaj lepszego sposobu na porównywanie z pominięciem wielkości znaków. 
         if wybor ==("CLOSE") or wybor == ('close'):
             print (end_literal)
+            #Importy umieszcza się na samej górze pliku, dzięki temu nie ukrywamy zależności w kodzie, wszystkie
+            #są widoczne na samej górze
             import sys
             sys.exit();
         if wybor == ("1"):        
@@ -130,25 +139,23 @@ def menu ():
             print (validation_input_corect_1)
             wybor = input()               
             print()
-    
-
-
-
-
-
-
-
+            
+#Nazwy funkcji powinny być w języku angielskim i powinny zawierać czasownik (funkcja to jakaś czynność)
 def liczba():
     print (welcome_literal, liczba_present1)
     print (nr_input, back_literal)
     podana_liczba = input('')
     liczba_logika(podana_liczba)
 
+    
+    
 def liczba_logika (podana_liczba):
     
     while True:
         try:
             if podana_liczba == (""):
+                #Korzystanie ze zmiennych globalnych utrudnia znacząco czytelność, 
+                #musiałbym skakać do góry żeby wiedzieć co tutaj jest wypisywane na ekranie
                 print (null_literal)
                 print (return_literal, next_number_error, back_literal, '\n')           
             elif podana_liczba.isspace():
@@ -162,6 +169,7 @@ def liczba_logika (podana_liczba):
                 print (return_literal, next_number_error, back_literal, '\n')
             
             else:
+                #Dużo powtórzeń, których można by się było pozbyć
                 wynik1 = float(podana_liczba) % 2         
                 if wynik1 == 0:
                     print (positive_literal[0:6], podana_liczba, positive_literal[-13:], '\n')
@@ -183,12 +191,6 @@ def liczba_logika (podana_liczba):
         return     
         
 
-
-
-
-
-
-
 def lista_wersja_1 ():
     print (welcome_literal, list_present1, '(WERSJA 1)')
     print (step_1_literal, back_literal)
@@ -196,6 +198,7 @@ def lista_wersja_1 ():
 
     while True:
         try:
+            #Walidacja poprawności danych wejściowych mogłaby być osobną funkcją
             if podana_dlugosc_listy == (""):
                 print (null_literal, '\n', refresh_literal)
             
@@ -210,19 +213,21 @@ def lista_wersja_1 ():
                 
             elif int(podana_dlugosc_listy) == 0:                                                                                
                 print (zero_validation, '\n', refresh_literal)
-                                         
+                                   
             elif int(podana_dlugosc_listy) > 0:                                                                                            
                 lista = []            
                 i = 0                                                                                          
                 print (step_2_literal)
                 decyzja = input('')
                 
+                #Skomplikowany warunek, powinen zostać nazwany i wydzielony jako osobna funkcja
                 while (decyzja != ('1') and decyzja != ('2') and decyzja != ('BACK') and decyzja != ('back')):
                     print (validation_input_1)
                     print (validation_input_corect_2)
                     decyzja = input()
                     print()
-                                  
+                
+                #Często używany warunek, też powinien być osobną funkcją
                 if decyzja ==("BACK") or decyzja ==("back") :
                     print (end_literal, list_present1)
                     menu()
@@ -257,6 +262,10 @@ def lista_wersja_1 ():
                             print ("        ", lista, '\n')
 
                             print ("    ", reverse_list_literal_1, '\n')
+                            
+                            #Spróbuj odwrócić tę listę sam, bez użycia funkcji z biblioteki standardowej 
+                            #i takich sztuczek. ;) Wiem, że normalnie zrobiłbyś to w taki sposób, ale chodzi mi
+                            #o to żebyście się nauczyli korzystać z pętli a nie z Google'a ;) 
 
                             print ("        ", reverse_list_literal_2, 'za pomocą [::-1]')
                             print ("        ", lista[::-1],'\n')
@@ -298,6 +307,9 @@ def lista_wersja_1 ():
         print()
 
 
+#Da się napisać uniwersalną funkcję do sortowania? 
+#Tak żebym podawał z góry argument "asc" -> sortowanie rosnące, "desc" -> sortowanie malejące
+#W tej chwili masz dwie metody, które różnią się tylko znakiem ">" / "<"
 def sortowanie_babelkowe_up(lista):
     zamiana = True
     while zamiana:
@@ -320,16 +332,6 @@ def sortowanie_babelkowe_down(lista):
                 lista[i] = lista[i+1]
                 lista[i+1] = tymczasowa
                 zamiana = True           
-
-
-
-
-
-
-
-
-
-
 
 def lista_wersja_2_1():
     print (welcome_literal, list_present1, '(WERSJA 2)')
@@ -367,13 +369,6 @@ def lista_2_2 ():
             #print (lista, '\n')        #wyświetla listę elementów po każdym dodaniu nowego wpisu
             print (back_literal_3)
             element = input()  
-
-
-
-
-
-
-
 
 def liczenie_znaków():
     print (welcome_literal, char_present1, '\n')
@@ -428,6 +423,26 @@ def liczenie_znaków_logika(text):
     return
         
 
+#Fajny pomysł na  te testy.
+#Test zazwyczaj robi się np. w sposób AAA: arrange, act, assert, na początku przygotowujesz sobie nie tylko dane
+#startowe, ale też oczekiwany rezultat jaki powinien być na koniec testu
+#Czyli przykładowo w Twoim przypadku, jak testujesz czy lista została odwrócona, to możesz sobie w teście wpisać
+#listę nieodwróconą i odwróconą, wykonać na liście nieodwróconej operację odwrócenia i porównać czy jej rezultatem
+#jest lista odwrócona. Przykładowy test w pseudokodzie poniżej. 
+
+
+#def test_revert_list():
+    #Arrange
+    #array = ['', ' ', 'test', "$@#@", '10', '11', '3.21']
+    #expected_result = ['3.21', '11', '10', "$@#@", 'test', ' ', '']
+
+    #Act
+    #reverted_list = revert_list(array)
+    
+    #Assert
+    #Sprawdzam czy operacja odwrócenia daje założony rezultat
+    #if not compare(reverted_list, expected_result)
+     #   print('Listy nie są takie same!')
 
 
 def test_1():    
